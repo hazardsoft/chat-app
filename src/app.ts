1cc635994d8eefd55e95337a4814826ab2fb2b62
Henadzi Shutko <hazardsoft@gmail.com>
2024-01-21 00:01:02 +0300

    feat: create express app serving "public"

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import { fileURLToPath } from "url";
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const publicPath = fileURLToPath(new URL("../public", import.meta.url));
+app.use(express.static(publicPath));
+
+app.use(cors());
+
+export { app };
